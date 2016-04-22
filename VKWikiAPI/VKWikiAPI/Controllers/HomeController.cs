a269ccea726deda504d9b24151a0510f798a53a9
newLink01 <korsakas-1996@mail.ru>
2016-04-22 16:05:55 +0300

    GetAccessToken

--- VKWikiAPI/VKWikiAPI/Controllers/HomeController.cs
+++ VKWikiAPI/VKWikiAPI/Controllers/HomeController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Net;
+using VKWikiAPI.Models;
 namespace VKWikiAPI.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+       
+        [HttpGet]
         public ActionResult Index()
         {
            
             return View();
         }
+
     }
 }
---
+++ VKWikiAPI/VKWikiAPI/Models/AccessToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VKWikiAPI.Models
+{
+    public class AccessToken
+    {
+        public string Value { set; get; }
+    }
+}
