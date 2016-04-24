924c71e7a1914670c7f010aee7c6287a4386dc24
newLink01 <korsakas-1996@mail.ru>
2016-04-24 21:51:00 +0300

    Get friends complite

---
+++ VKWikiAPI/VKWikiAPI/Classes/VKWikiFunctional.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace VKWikiAPI.Classes
+{
+    public class VKWikiFunctional
+    {
+        public string VKGetStatus(string accessToken) {
+            if (accessToken == null) { return null; }
+            string responseFromServer = null;
+            
+            string url = "https://api.vk.com/method/status.get?user_id=166724944&access_token=" + accessToken;
+            WebRequest req = WebRequest.Create(url);
+            WebResponse resp = req.GetResponse();
+            
+
+
+            using (Stream dataStream = resp.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    responseFromServer = reader.ReadToEnd();
+                }
+            }
+            return responseFromServer;
+        }
+
+        public object VKGetWall(string accessToken) {
+            if (accessToken == null) { return null; }
+            string responseFromServer = null;
+
+            string url = "https://api.vk.com/method/status.get?user_id=166724944&access_token=" + accessToken;
+            WebRequest req = WebRequest.Create(url);
+            WebResponse resp = req.GetResponse();
+
+
+
+            using (Stream dataStream = resp.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    responseFromServer = reader.ReadToEnd();
+                }
+            }
+            return responseFromServer;
+
+
+        }
+
+        public string VKGetFriends(string accessToken) {
+            List<string> friends = new List<string>();
+
+            string responseFromServer = null;
+            string url = "https://api.vk.com/method/friends.get?user_id=166724944&access_token=" + accessToken;
+
+            WebRequest req = WebRequest.Create(url);
+            WebResponse resp = req.GetResponse();
+
+
+
+            using (Stream dataStream = resp.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    responseFromServer = reader.ReadToEnd();
+                }
+            }
+            return responseFromServer;
+        }
+
+
+
+
+    }
+}
--- VKWikiAPI/VKWikiAPI/Controllers/HomeController.cs
+++ VKWikiAPI/VKWikiAPI/Controllers/HomeController.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using VKWikiAPI.Classes;
 namespace VKWikiAPI.Controllers
 {
     public class HomeController : Controller
     {
-        private string AccessToken { set; get; } = "blabla";
+        public static string AccessToken { set; get; } 
+        private VKWikiFunctional functional;
+
+        public HomeController() {
+            functional = new VKWikiFunctional();
+        }
 
         [HttpGet]
         public ActionResult Index()
@@ -17,13 +22,19 @@ namespace VKWikiAPI.Controllers
         }
 
         [HttpGet]
-        public ActionResult GetToken(string accessToken) {
-            this.AccessToken = accessToken;
+        public ActionResult SetToken(string accessToken) {
+           AccessToken = accessToken;
             return View();
         }
 
+        [HttpGet]
+        public string GetStatus() {
+            return functional.VKGetStatus(AccessToken); 
+        }
 
-
-
+        [HttpGet]
+        public string GetFriends() {
+            return functional.VKGetFriends(AccessToken);
+        }
     }
 }
